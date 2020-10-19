19927b8f35de032d07e3d785af1d1e6df8753fad
darylemb <58637116+darylemb@users.noreply.github.com>
2020-10-19 15:28:02 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,6 +4,11 @@ sudo apt install git
 sudo apt install gcc
 sudo apt install g++
 sudo apt update
+if [ -d ns-3-allinone ]
+then 
+	echo "Borrando carpeta preexistente"
+	rm -r ns-3-allinone
+fi
 git clone https://gitlab.com/nsnam/ns-3-allinone.git
 cd ns-3-allinone
 python3 download.py
