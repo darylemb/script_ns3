1732fd60432817789a0ea48c218431697d1c431d
darylemb <58637116+darylemb@users.noreply.github.com>
2020-10-21 13:49:10 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ~
+sudo apt install git
+sudo apt install gcc
+sudo apt install g++
+sudo apt update
+if [ -d ns-3-allinone ]
+then 
+	echo "Borrando carpeta preexistente"
+	rm -r ns-3-allinone
+fi
+git clone https://gitlab.com/nsnam/ns-3-allinone.git
+cd ns-3-allinone
+python3 download.py
+cd ns-3-dev
+./waf clean
+CXXFLAGS="-W -Wall -g" ./waf configure --disable-examples --disable-tests
+./waf build
